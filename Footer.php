5f6f427702cb55d79e9c4db3927b98e29fbb8def
ChristinaAndrian <christina.andrian@gmail.com>
2015-10-30 12:09:41 +0100

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,28 @@
+<!--	<br style="clear: both;" /> -->
+	<div id="footer">
+		<div id="contactbox">
+			<!-- disable photo logo
+			<a href="http://www.ths.kth.se"><img src="<?=bloginfo('template_url');?>/images/THS_foot_logo.png" class="logo" alt="Tekniska h&ouml;gskolans studentk&aring;r" /></a>
+			-->
+			<img src="<?=bloginfo('template_url');?>/images/footer_text.png" class="text" alt="N&auml;ringsliv" />
+			<!--disable aman's email  
+			<p><span><?php _e('THS N&auml;ringslivschef');?></span>
+				Aman Mebrahtu<br />
+				+46(0)8 - 790 98 86<br />
+				naringsliv@ths.kth.se<br />
+			</p>
+		-->
+		</div>
+		<div id="login">
+			<a href="http://www.ths.kth.se">Tekniska H&ouml;gskolans Studentk&aring;r</a>, org nr: 802005-9153, is proudly powered by <a href="http://wordpress.org">WordPress</a>. <a href="<?php echo get_admin_url();?>">Logga in</a> 
+		</div> 
+	</div>
+</div>
+
+<div id="fb-root"></div>
+<?php
+wp_footer();
+?>
+
+</body>
+</html>
